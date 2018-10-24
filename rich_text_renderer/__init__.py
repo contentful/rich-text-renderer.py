50905562bff18caa8a4f7bbb9a41b2d56c4d37ec
David Litvak <david.litvakb@gmail.com>
2018-10-24 10:49:23 +0200

    Bump to version 0.2.0

--- rich_text_renderer/__init__.py
+++ rich_text_renderer/__init__.py
@@ -1,6 +1,6 @@
 from .rich_text_renderer import RichTextRenderer  # noqa: F401
 
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Contentful GmbH (David Litvak Bruno)"
 __email__ = "david.litvak@contentful.com"
