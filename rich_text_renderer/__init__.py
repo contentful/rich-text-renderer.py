7fd0420b6fea074f2643fe8330df0b6ac9ec8519
Janko Marklein <janko.marklein@contentful.com>
2022-10-13 10:29:00 +0200

    Bump to version 0.2.7

--- rich_text_renderer/__init__.py
+++ rich_text_renderer/__init__.py
@@ -1,6 +1,6 @@
 from .rich_text_renderer import RichTextRenderer  # noqa: F401
 
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 __author__ = "Contentful GmbH (David Litvak Bruno), Bhushan Lodha (rubydog)"
 __email__ = "bhushan.lodha@external.contentful.com"
